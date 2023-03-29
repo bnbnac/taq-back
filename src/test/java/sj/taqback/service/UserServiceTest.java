a02fddf3254cfac7918b9dda6d19ccd2722a92cb
bnbnac <bnbnac@naver.com>
2023-03-29 20:04:09 +0900

    test: create user account, find user by account id, find user by nickname

--- src/test/java/sj/taqback/service/UserServiceTest.java
+++ src/test/java/sj/taqback/service/UserServiceTest.java
@@ -1,2 +1,33 @@
-package sj.taqback.service;public class UserServiceTest {
+package sj.taqback.service;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import sj.taqback.domain.User;
+import sj.taqback.repository.UserRepository;
+
+import java.time.LocalDateTime;
+
+@SpringBootTest
+@Transactional
+public class UserServiceTest {
+    @Autowired
+    UserService userService;
+    @Autowired
+    UserRepository userRepository;
+
+    @Test
+    void createAccountAndFindUser() {
+        User user = new User();
+        user.setAccountId("bnac");
+        user.setNickname("sj");
+
+        userService.createAccount(user);
+        User foundUserByAccountId = userService.findByAccountId("bnac").get();
+        User foundUserByNickname = userService.findByNickname("sj").get();
+        Assertions.assertThat(user.getNickname()).isEqualTo(foundUserByAccountId.getNickname());
+        Assertions.assertThat(user.getAccountId()).isEqualTo(foundUserByNickname.getAccountId());
+    }
 }
