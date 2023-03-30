517a6e6abdbb610c781c297c6d8baa8588e707ca
bnbnac <bnbnac@naver.com>
2023-03-30 19:57:07 +0900

    feat: disable SecurityAutoConfig

--- src/main/java/sj/taqback/TaqBackApplication.java
+++ src/main/java/sj/taqback/TaqBackApplication.java
@@ -2,8 +2,9 @@ package sj.taqback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class TaqBackApplication {
 
 	public static void main(String[] args) {
