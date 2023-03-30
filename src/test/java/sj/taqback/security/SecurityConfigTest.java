87bb6d10a4a9fe8aeb392a909495fac04b249363
bnbnac <bnbnac@naver.com>
2023-03-30 15:46:25 +0900

    test: test generate passwordEncoder with properly secure strength parameter

--- src/test/java/sj/taqback/security/SecurityConfigTest.java
+++ src/test/java/sj/taqback/security/SecurityConfigTest.java
@@ -1,12 +1,22 @@
-package sj.taqback.config;
+package sj.taqback.security;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import sj.taqback.config.SecureBCryptPasswordEncoder;
 
 public class SecurityConfigTest {
-    @Test
-    void getSafeStrengthForBCryptPasswordEncoder() {
+    int MAX_STRENGTH_SUPPORTED_BCRYPT_PASSWORD_ENCODER = 31;
+    int LOW_LIMIT = 5;
 
+    @Test
+    void createBCryptPasswordEncoder() {
+        new SecureBCryptPasswordEncoder(4, MAX_STRENGTH_SUPPORTED_BCRYPT_PASSWORD_ENCODER);
+    }
+
+    @Test
+    void giveBCryptPasswordVeryLowLimitStrength() {
+        Assertions.assertThatThrownBy(() -> new SecureBCryptPasswordEncoder(4, LOW_LIMIT))
+                .isInstanceOf(RuntimeException.class);
     }
 
 }
