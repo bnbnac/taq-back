3219e8fabbc534740295065fad44c6bb48a97378
bnbnac <bnbnac@naver.com>
2023-05-17 16:12:31 +0900

    test: signup api test

--- src/test/java/sj/taqback/api/SignupApiTest.java
+++ src/test/java/sj/taqback/api/SignupApiTest.java
@@ -1,25 +1,52 @@
-package sj.taqback.controller;
+package sj.taqback.api;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
+import sj.taqback.controller.SignupForm;
+import sj.taqback.domain.User;
+import sj.taqback.repository.UserRepository;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@AutoConfigureMockMvc
 @Transactional
 public class SignupApiTest {
-    @Test
-    void signupApi() {
-        TestRestTemplate rest = new TestRestTemplate();
-
-        ResponseEntity<String> res =
-                rest.getForEntity()
-    }
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    UserRepository userRepository;
 
     @Test
-    void createDuplicatedAccountIdApi() {
+    public void testCreateUser() throws Exception {
+        SignupForm form = createForm("id", "pw", "nick");
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonRequest = objectMapper.writeValueAsString(form);
 
+        mockMvc.perform(post("/users/new")
+                        .contentType("application/json")
+                        .content(jsonRequest));
+
+        User user = userRepository.findByAccountId("id").get();
+        String nickname = user.getNickname();
+
+        Assertions.assertThat(nickname).isEqualTo("nick");
     }
 
+    SignupForm createForm(String accountId, String password, String nickname) {
+        SignupForm form = new SignupForm();
+        form.setAccountId(accountId);
+        form.setPassword(password);
+        form.setNickname(nickname);
+
+        return form;
+    }
 }
+
+
